e6b5773700239077071a9e0a5be4b749bb4eef3a
n-wilbanks <31506967+n-wilbanks@users.noreply.github.com>
2025-02-04 13:14:42 -0800

    Added extra functionality
    Added additional functionality to give this more use cases:
     - If jobs.txt is empty, option 1 will now repopulate the list rather than erroring out
     - Removed all hard-coded references to "jobs.txt" and replaced them with the "path" variable
     - Updated descriptions in the menu and explanation of the application

--- Roulette_Randomizer.py
+++ Roulette_Randomizer.py
@@ -17,8 +17,8 @@ def main():
     # Main Menu
     print("Non-Mentors will probably want Option 2.")
     print("Please make a selection: ")
-    print("    1: Mentor Roulette")
-    print("    2: Choose my job")
+    print("    1: Job Streak Roulette (a file named 'jobs.txt' will be created where this program is saved)")
+    print("    2: Choose my job at random (no files created)")
     print("    3: Explain the options")
     print("  Q/q: Quit this application")
     
@@ -37,9 +37,10 @@ def main():
             exit()
         case '3':
             print("\nOption 1 on a first run will import every job in the game into a file, read it,\nselect a job for you, and remove that job from the list before saving the file. \nThis is to ensure you cannot receive that job again when selecting this option.")
-            print("Therefore you are intended to run around 100 Mentor Roulettes with this job to assist in job mastery.")
             print("On a second run, Option 1 will read the file saved as 'jobs.txt', import all jobs within that file,\nand select from those.")
             print("If jobs.txt does not exist, this will run as if it were a first run, so be careful not to move or delete the file!\n")
+            print("Option 1 will check to see if jobs.txt is empty on every run.  If it is, it will repopulate the file.\n")
+            print("If you are looking to do streaks of varying length with a randomized job, this option is for you.\n")
             print("Option 2 will select a random job for you and the application will automatically close after 5 seconds.\n")
             print("Option 3 will display this menu and return to the main menu after 5 seconds.\n")
             print("Q or q will immediately quit the application.\n")
@@ -49,18 +50,23 @@ def main():
             print("Exiting...")
             exit()
     
-# Choose a job and remove it from the list - Specifically intended for Mentor Roulette streaks to encourage job mastery
+# Choose a job and remove it from the list - Previously intended for Mentor Roulette streaks to encourage job mastery
 def chooseAndRemove():
-    print("Checking for 'jobs.txt'...")
+    print("Checking for", path)
     time.sleep(3)
     # If jobs.txt exists, reads in the file and picks a job from what's left
     if path.exists():
         try:
             jobsLeft = ""
-            with open("jobs.txt", "r") as fi:
+            # Check if jobs is empty. If yes, repopulate the list
+            if os.stat(path).st_size == 0:
+                with open(path, "w") as fil:
+                    fil.write("\n".join(jobs))
+                    fil.close()
+            with open(path, "r") as fi:
                 jobsLeft = fi.read().splitlines()
                 fi.close()
-            with open("jobs.txt", "w") as f:
+            with open(path, "w") as f:
                 jobChosen = system_random.choice(jobsLeft)
                 print(jobChosen)
                 jobsLeft.remove(jobChosen)
@@ -74,7 +80,7 @@ def chooseAndRemove():
     # If jobs.txt does not exist, create a file with all jobs and select from those
     if not path.exists():
         try:
-            with open("jobs.txt", "w") as f:
+            with open(path, "w") as f:
                 jobChosen = system_random.choice(jobs)
                 print(jobChosen)
                 jobs.remove(jobChosen)
@@ -86,7 +92,7 @@ def chooseAndRemove():
             time.sleep(3)
             exit()
 
-# Just choose a job - Intended for everything other than Mentor Roulette streaks
+# Just choose a job - Intended for everything other than job streaks
 def justChoose():
     print(system_random.choice(jobs))
 
